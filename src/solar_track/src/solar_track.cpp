3fb81ff5da0329393e2f2f9a2d47a61fe8b6b7eb
Hardy Nelson <arlen.nelson@gmail.com>
2019-09-08 19:05:50 -0700

    aquiring system time complete

--- src/solar_track/src/solar_track.cpp
+++ src/solar_track/src/solar_track.cpp
@@ -1,7 +1,11 @@
 #include "ros/ros.h"
-#include "std_msgs/Float32.h"  //we will have to check on the MCUs capability
+#include "ros/console.h"
+#include "std_msgs/Float32.h"  // We will have to check on the MCUs capability
 
-extern "C" 
+#include <ctime>
+#include <cstdlib>
+
+extern "C" // Required for C to be useable within C++
 {
   #include "SolTrack.h" //solar tracking library
 }
@@ -47,7 +51,7 @@ int main(int argc, char **argv)
    */
   ros::Publisher solar_position_pub = nh.advertise<std_msgs::Float32>("solar_position", 1);
 
-  ros::Rate loop_rate(10);  // seconds
+  ros::Rate loop_rate(1);  // Hz
 
   /**
    * A count of how many messages we have sent. This is used to create
@@ -56,64 +60,85 @@ int main(int argc, char **argv)
   int count = 0;
   while ( ros::ok() )
   {
+    // TODO According to ROS docs. this might need to be gaurded against a 0 value!
+    std::time_t epochNow = ros::Time::now().toSec();
+
+    std::tm localTimeNow = *localtime(&epochNow);
+    std::tm utcTimeNow = *gmtime(&epochNow);
+
+    // DEBUG: Print various components of tm structure.
+    
+    ROS_INFO_STREAM("Local Time/date as reported by system: "
+      << 1900 + localTimeNow.tm_year << "-" // Years since 1900
+      << 1 + localTimeNow.tm_mon << "-"  // A number of tm fields are 0 indexed
+      << localTimeNow.tm_mday << ","
+      << 1 + localTimeNow.tm_hour << ":"
+      << 1 + localTimeNow.tm_min << ":"
+      << 1 + localTimeNow.tm_sec << ","
+      << "DST:" << localTimeNow.tm_isdst << "," 
+      << localTimeNow.tm_zone << ";"
+      << "Relative UTC corrected for DST:" << 1 + utcTimeNow.tm_hour - 1 );
+
     /**
-     * This is a message object. You stuff it with data, and then publish it.
+     * Populate the SolTrack structures
      */
 
     int useDegrees = 1;             // Input (geographic position) and output are in degrees
-    int useNorthEqualsZero = 1;     // Azimuth: 0 = South, pi/2 (90deg) = West  ->  0 = North, pi/2 (90deg) = East
-    int computeRefrEquatorial = 1;  // Compure refraction-corrected equatorial coordinates (Hour angle, declination): 0-no, 1-yes
+    int useNorthEqualsZero = 1;     // Azimuth: 0 = South, pi/2 (90deg) = West w.  0 = North, pi/2 (90deg) = East
+    int computeRefrEquatorial = 0;  // Compure refraction-corrected equatorial coordinates (Hour angle, declination): 0-no, 1-yes
     int computeDistance = 1;        // Compute the distance to the Sun in AU: 0-no, 1-yes
-    
-    
-    struct Time time;
-    
-    // Set (UT!) date and time manually - use the first date from SolTrack_positions.dat:
-    time.year = 2019;
-    time.month = 9;
-    time.day = 7;
-    time.hour = 15;  // 10h PDST + 7 = 15h UT
-    time.minute = 0;
-    time.second = 0.00;
+      
+    struct Time time; // "Time" is probably a pretty dangerous struct name for the SolTrack author to be using
+
+    time.year = 1900 + localTimeNow.tm_year;
+    time.month = 1 + localTimeNow.tm_mon;
+    time.day = localTimeNow.tm_mday;
+
+    if (localTimeNow.tm_isdst)  // Handle DST
+    {
+      time.hour = (1 + utcTimeNow.tm_hour) - 1; // UTC!
+    }
+
+    time.minute = 1 + localTimeNow.tm_min;
+    time.second = 1 + localTimeNow.tm_sec; //SolTrack resolves to micro-seconds, ctime does not. This is good enough.
     
     struct Location loc;
+
     loc.latitude =  49.2819798;  // 417 Carlen Pl.
     loc.longitude  = -122.82307848;
-    loc.pressure = 101.0;     // Atmospheric pressure in kPa
-    loc.temperature = 283.0;  // Atmospheric temperature in K
+
+    loc.pressure = 101.325;  // Atmospheric pressure in kPa @STP, we aren't using refraction anyway (see above)
+    loc.temperature = 273.0;  // Atmospheric temperature in K @STP, we aren't using refraction anyway (see above)
     
     // Compute rise and set times:
     struct Position pos;
     struct RiseSet riseSet;
-    SolTrack_RiseSet(time, loc, &pos, &riseSet, 0.0, useDegrees, useNorthEqualsZero);
+    SolTrack_RiseSet(time, loc, &pos, &riseSet, 0.0, useDegrees, useNorthEqualsZero);  // 5th term (0.0) is altitude (assume meters? It's not clear)
     
     // Compute positions:
     SolTrack(time, loc, &pos, useDegrees, useNorthEqualsZero, computeRefrEquatorial, computeDistance);
     
-    // Write data to screen:
-    printf("Date:   %4d %2d %2d\n", time.year, time.month, time.day);
-    printf("Time:   %2d %2d %9.6lf\n", (int)time.hour, (int)time.minute, time.second);
-    printf("JD:     %20.11lf\n\n", pos.julianDay);
-    
-    printf("Rise time:      %11.5lf,    azimuth:   %11.5lf\n", riseSet.riseTime, riseSet.riseAzimuth);
-    printf("Transit time:   %11.5lf,    altitude:  %11.5lf\n", riseSet.transitTime, riseSet.transitAltitude);
-    printf("Set time:       %11.5lf,    azimuth:   %11.5lf\n\n", riseSet.setTime, riseSet.setAzimuth);
-    
-    printf("Ecliptic longitude, latitude:        %10.6lf° %10.6lf°\n", pos.longitude, 0.0);
-    printf("Right ascension, declination:        %10.6lf° %10.6lf°\n", pos.rightAscension, pos.declination);
-    printf("Uncorrected altitude:                            %10.6lf°\n\n", pos.altitude);
-    printf("Corrected azimuth, altitude:         %10.6lf° %10.6lf°\n", pos.azimuthRefract, pos.altitudeRefract);
-    printf("Corected hour angle, declination:    %10.6lf° %10.6lf°\n\n", pos.hourAngleRefract, pos.declinationRefract);
-    
+    /**
+     * Populate ROS message with SolTrack structures
+     */
+
+
+
+
+
+    /**
+     * This is a message object. You stuff it with data, and then publish it.
+     */
+
+    std_msgs::Float32 riseTime;
+    riseTime.data = riseSet.riseTime;
+
     /**
      * The publish() function is how you send messages. The parameter
      * is the message object. The type of this object must agree with the type
      * given as a template parameter to the advertise<>() call, as was done
      * in the constructor above.
      */
-
-    std_msgs::Float32 riseTime;
-    riseTime.data = riseSet.riseTime;
 
     solar_position_pub.publish(riseTime);
 
